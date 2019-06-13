b8dcb1ed10a85ba70dd066f5219d72a8659d04fc
Lucas Brandstaetter <lucas@brandstaetter.tech>
2019-06-14 01:50:48 +0200

    Improve luacheck action
    * Add error checking when downloading luacheckrc
    * Provide better feedback about current progress

--- luacheck/entrypoint.sh
+++ luacheck/entrypoint.sh
@@ -1,16 +1,24 @@
 #!/bin/sh
 
-echo "${LUACHECKRC_URL}"
-if [ -z "$LUACHECKRC_URL" ]
-then
-  ls -la
-  if [ ! -f .luacheckrc ]
-  then
+if [ -z "$LUACHECKRC_URL" ]; then
+  if [ ! -f .luacheckrc ]; then
     echo "ERROR: No .luacheckrc present and no \$LUACHECKRC_URL provided" 1>&2
     exit 1
   fi
 else
-  curl $LUACHECKRC_URL -o .luacheckrc
+  echo "Downloading luacheckrc file from ${LUACHECKRC_URL}"
+  HTTP_RESPONSE=$(curl --write-out "HTTPSTATUS:%{http_code}"\
+    -sSL \
+    -o .luacheckrc \
+    $LUACHECKRC_URL )
+
+  HTTP_STATUS=$(echo $HTTP_RESPONSE | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
+  if [ $HTTP_STATUS -ne 200 ]; then
+    echo "Failed to download .luacheckrc file"
+    exit 1
+  fi
 fi
+
+echo "Checking all lua files"
 luacheck .
 

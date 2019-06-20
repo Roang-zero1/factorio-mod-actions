77abac061f592608f8bd8b0b888135104b59ce58
Lucas Brandstaetter <lucas@brandstaetter.tech>
2019-06-20 14:42:42 +0200

    Fix linting

--- docker-base/factorio-mod/update.sh
+++ docker-base/factorio-mod/update.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -eo pipefail
 
-
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
-versions=( */ )
-for version in "${versions[@]%/}" ; do
-  oses=( $(ls ${version}) )
-  for os in "${oses[@]}" ; do
+versions=(*/)
+for version in "${versions[@]%/}"; do
+  oses=($(ls ${version}))
+  for os in "${oses[@]}"; do
     image=roangzero1/factorio-mod:${version}-${os}
     echo building $image ...
     docker build -q -t ${image} ${version}/${os}
--- docker-base/lua/update.sh
+++ docker-base/lua/update.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -eo pipefail
 
-
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
-versions=( */ )
-for version in "${versions[@]%/}" ; do
-  oses=( $(ls ${version}) )
-  for os in "${oses[@]}" ; do
+versions=(*/)
+for version in "${versions[@]%/}"; do
+  oses=($(ls ${version}))
+  for os in "${oses[@]}"; do
     image=roangzero1/lua:${version}-${os}
     echo building $image ...
     docker build -q -t ${image} ${version}/${os}

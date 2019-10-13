f255250317f2008acf8096fbf5e806a68633525f
Lucas Brandstaetter <lucas@brandstaetter.tech>
2019-10-13 02:44:12 +0200

    Fix typos in variables

--- docker-base/update.sh
+++ docker-base/update.sh
@@ -4,24 +4,24 @@ set -euo pipefail
 cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 base="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
-reposiroties=(
+repositories=(
   "lua:lua -v"
   'luarocks:luarocks'
   'factorio-mod:luacheck -v'
 )
 
-for reposiroty_config in "${reposiroties[@]}"; do
-  reposiroty=${reposiroty_config%%:*}
-  echo "Building images of ${reposiroty}"
-  cd ${reposiroty}
+for repository_config in "${repositories[@]}"; do
+  repository=${repository_config%%:*}
+  echo "Building images of ${repository}"
+  cd ${repository}
   versions=(*/)
   for version in "${versions[@]%/}"; do
     oses=($(ls ${version}))
     for os in "${oses[@]}"; do
-      image=roangzero1/${reposiroty}:${version}-${os}
+      image=roangzero1/${repository}:${version}-${os}
       echo "building $image ..."
       docker build -q -t ${image} ${version}/${os}
-      docker run --rm ${image} ${reposiroty_config#*:}
+      docker run --rm ${image} ${repository_config#*:}
     done
   done
   cd ${base}
